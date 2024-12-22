b41704979a01fffad6f4c51fea6f153226c5a5ef
Keval Parmar <thekevalparmar@gmail.com>
2024-12-22 16:40:30 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 220210
+let accountEmail = "kp@gmail.com"
+var accountPassword = "1234"
+accountCity = "ahmedabad"
+
+// accountId = 22021 //not allowed beacuse 
+// accountId is declared with const keyword
+
+accountEmail = "kp@kp.com"
+accountPassword = "2468"
+accountCity = "Banglore"
+
+let accountState;
+console.log(accountId);
+/*
+ prefer not to use var,
+ problem in block scope and function scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+//const
+//let
+//print table using console.log([])

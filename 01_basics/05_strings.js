dc3abcd60f790bab523b47129195559c1bea18f2
Keval Parmar <thekevalparmar@gmail.com>
2024-12-26 12:16:52 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,48 @@
+const name1 = "keval"
+const repoCount1 = 100
+
+console.log(name+repoCount1+" Value");
+
+console.log(`Hello i am ${name1} and my repo value is ${repoCount1} value`)
+
+const gameName = new String("keval-kp-com")
+console.log(gameName.toString());
+console.log(gameName.at(3));
+
+//chai aur code
+
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName1 = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));

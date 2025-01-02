8787cd3bca9064b5fc33e183a400de4d9a290fae
Keval Parmar <thekevalparmar@gmail.com>
2025-01-02 17:31:44 +0000

    loops discussion

---
+++ 05_iteration/two.js
@@ -0,0 +1,28 @@
+let index = 0
+while (index <= 10) {
+    // console.log(index);
+    
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arrIndex = 0
+while (arrIndex < myArray.length) {
+    console.log(`Value of myArray is ${myArray[arrIndex]}`);
+    
+    // console.log(`Value is ${myArray[arr]}`);
+    arrIndex = arrIndex + 1
+}
+
+
+// let score = 11
+// let score = 10
+let score = 5
+
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
